1ccb4bf6c64d721c56209ff763f4fb9590243ca8
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-07-09 00:42:46 -0700

    fetches plant data and renders on screen

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -9,11 +9,21 @@ export default class PlantList extends Component {
   //   - set the returned plants array to this.state.plants
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+
+  state = {
+    plants: [],
+  }
+
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        console.log(res);
+        this.setState({
+        plants: res.data
       });
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   render() {
